33d6dab10916230694c269f13b544fecee6441a6
Joey Janmaat <Joey.janmaat@student.rocva.nl>
2021-05-20 12:47:10 +0200

    antwoorden

--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,3 @@
-
+INSERT INTO `studenten2` (`voornaam`, `achternaam`, `woonplaats`) VALUES ('Hugo', 'Boss', 'Utrecht');
+INSERT INTO `studenten2` (`voornaam`, `achternaam`, `woonplaats`) VALUES ('Jippie', 'Janneke', 'Utrecht');
+INSERT INTO `studenten2` (`voornaam`, `achternaam`, `woonplaats`) VALUES ('Freek', 'Vonk', 'Amsterdam');
