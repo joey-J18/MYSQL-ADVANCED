d862584d0bcb30e815e6eedd3f14a25976b8efcc
Joey Janmaat <Joey.janmaat@student.rocva.nl>
2021-05-25 11:13:48 +0200

    opdracht 3

--- 2-UPDATE/taak01 - Update/antwoorden.sql
+++ 2-UPDATE/taak01 - Update/antwoorden.sql
@@ -1,9 +1,26 @@
 -- Opdracht 1
-
+UPDATE studenten2 SET woonplaats = 'Amstelveen'
 -- Opdracht 2 
+UPDATE studenten2
+SET adres = 'brooklynstraat 82' , postcode = '1230 DD'
+WHERE student_id = 8;
 
+UPDATE studenten2
+SET adres = 'beerninckstraat 82' , postcode = '1500 DD'
+WHERE student_id = 9;
+
+UPDATE studenten2
+SET adres = 'beatrixstraat 69' , postcode = '2340 DD'
+WHERE student_id = 10;
 -- Opdracht 3
-
+UPDATE studenten2 SET geboortedatum = '2000-12-12' WHERE student_id = 2
 -- Opdracht 4
+UPDATE studenten2
+SET klas = '9C'
+WHERE student_id = 4;
 
+UPDATE studenten2
+SET klas = '9A'
+WHERE student_id = 8;
 -- Opdracht 5
+UPDATE studenten2 SET voornaam = 'Johannah' WHERE student_id = 6
